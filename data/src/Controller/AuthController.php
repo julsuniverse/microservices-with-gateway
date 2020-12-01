f0dbb25ef2a30ed55eb1aa28b50fb8be38374c99
julia <julsuniverse@gmail.com>
2020-12-01 18:23:21 +0200

    add registration

---
+++ data/src/Controller/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Model\User\UseCase\Register;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    public function __construct(SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+    }
+
+    /**
+     * @Route("/register", name="register", methods={"POST"})
+     * @param Request $request
+     * @param Register\Handler $handler
+     * @return JsonResponse
+     */
+    public function register(Request $request, Register\Handler $handler): JsonResponse
+    {
+        /** @var Register\Command $command */
+        $command = $this->serializer->deserialize($request->getContent(), Register\Command::class, 'json');
+
+        $violations = $this->validator->validate($command);
+        if (\count($violations)) {
+            $json = $this->serializer->serialize($violations, 'json');
+            return new JsonResponse($json, Response::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $handler->handle($command);
+
+        return $this->json([], Response::HTTP_CREATED);
+    }
+}
---
+++ data/src/Model/User/UseCase/Register/Handler.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\Register;
+
+use App\Model\User\Entity\Id;
+use App\Model\User\Entity\User;
+use App\Model\User\Entity\UserRepository;
+use App\Model\User\Service\PasswordGenerator;
+use App\Model\User\Service\PasswordHasher;
+use Doctrine\ORM\EntityManagerInterface;
+
+class Handler
+{
+    /**
+     * @var PasswordHasher
+     */
+    private $passwordsHasher;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        PasswordHasher $passwordsHasher,
+        UserRepository $userRepository
+    ) {
+        $this->passwordsHasher = $passwordsHasher;
+        $this->em = $em;
+        $this->userRepository = $userRepository;
+    }
+
+    public function handle(Command $command)
+    {
+        if ($this->userRepository->hasByEmail($command->email)) {
+            throw new \DomainException('User with such email already exists.');
+        }
+
+        $user = new User(
+            Id::next(),
+            $command->email,
+            $this->passwordsHasher->hash($command->email),
+            new \DateTimeImmutable()
+        );
+
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+}
